b8ec5146c7633472160c76ca61cc46f56bfc8d10
Marcelo Araújo <marceloguitarst@gmail.com>
2016-06-27 23:38:36 -0300

    mais detalhes da descricao do projeto

--- app/Http/Controllers/ProjetoController.php
+++ app/Http/Controllers/ProjetoController.php
@@ -15,13 +15,37 @@ use DB;
 class ProjetoController extends Controller
 {
     public function show ($id, $id2) {
-    	$desc = DB::select('select distinct p.criador, p.nome, p.desc from vereador v, projetos p, ver_proj vp where
-         p.id = vp.id_proj and vp.id_ver = v.id and ? = vp.id_ver and ? = vp.id_proj', [$id, $id2]);
 
-    	$qtdV = DB::select('select COUNT(vt.tipo) as qt from vereador v, projetos p, votos vt where
-    	 p.id = vt.id_proj and v.id = vt.id_ver and p.id = ?', [$id2]);
+         //Retorna o total de votos
+    	$desc = DB::select('select COUNT(vt.tipo) as qt, p.nome, p.desc, p.criador from vereador v, projetos p, votos vt where 
+    		p.id = vt.id_proj and v.id = vt.id_ver and p.id = ?', [$id2]);
 
-    	return view('projeto.descProj', ['desc' => $desc, 'qtdV' => $qtdV]);
+    	    	//Votos sim 
+    	$desc2 = DB::select('select COUNT(vt.tipo) as qt from vereador v, projetos p, votos vt where 
+    		p.id = vt.id_proj and v.id = vt.id_ver and p.id = ? and vt.tipo = 1', [$id2]);
+    	//Votos não
+    	$desc3 = DB::select('select COUNT(vt.tipo) as qt from vereador v, projetos p, votos vt where 
+    		p.id = vt.id_proj and v.id = vt.id_ver and p.id = ? and vt.tipo = 2', [$id2]);
+    	//Abnegação
+    	$desc4 = DB::select('select COUNT(vt.tipo) as qt from vereador v, projetos p, votos vt where 
+    		p.id = vt.id_proj and v.id = vt.id_ver and p.id = ? and vt.tipo = 3', [$id2]);
+
+
+    	return view('projeto.descProj', ['desc' => $desc, 'desc2' => $desc2, 'desc3' => $desc3, 'desc4' => $desc4]);
+
+    
     }
 }
 
+
+
+
+
+    	/*
+    	$desc = DB::select('select distinct p.criador, p.nome, p.desc from vereador v, projetos p, ver_proj vp where
+         p.id = vp.id_proj and vp.id_ver = v.id and ? = vp.id_ver and ? = vp.id_proj', [$id, $id2]);
+*/
+
+
+
+         	/**/
--- resources/views/projeto/descProj.blade.php
+++ resources/views/projeto/descProj.blade.php
@@ -5,13 +5,22 @@
 	@foreach ($desc as $d)
 		<h2>{{$d->nome}}</h2>
 		<p>{{$d->desc}}</p>
-		
-		Criador: {{$d->criador}}
+		Criador: {{$d->criador}} <br>
+	@endforeach
+	<br>
+	@foreach ($desc2 as $d2)
+		Votos Sim {{$d2->qt}}
+	@endforeach
+	<br>
+	@foreach ($desc3 as $d3)
+		Votos Não {{$d3->qt}}
+	@endforeach
+	<br>
+	@foreach ($desc4 as $d4)
+		Abstinencia {{$d4->qt}}
 	@endforeach
 
-	<p>
-		
-	</p>
+
 
 
 @stop
